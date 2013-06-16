20ac833c5e329d12df6baaf78c8e73857445f6ce
niboss <niboss@niboss-mac.local>
2013-06-16 14:25:54 +0900

    add hello.rb

--- test.rb
+++ test.rb
@@ -1,2 +1,28 @@
-p "test"
+include Math 
+puts sin(3.1415)
+puts (20+1)/1
+
+a=20
+if(a>=100) then
+    print "bigger\n"
+end
+if a>9 
+    print "small"
+end
+
+i=1
+while i<=10
+    print i, "\n"
+    i+=1
+end
+
+100.times do
+  puts "a"
+  end
+
+def hello
+    print "hallo,ruby.\n"
+end
+
+hello()
 
